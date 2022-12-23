e1b4080f1e45449497c1565d85b26f3c854a5b6a
chenhonzhou <chenhonzhou@gmail.com>
2022-12-23 20:25:03 +0800

    feat: support search keyword
    Co-Authored-By: Yoso <111450719+yosokim@users.noreply.github.com>

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,6 +7,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
 
+typedef ColCookList = Map<String, List<Before>>;
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -15,26 +17,77 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  CookParseDataModel? data;
+  final AsyncMemoizer _memoizer = AsyncMemoizer();
+  final TextEditingController _textEditingController = TextEditingController();
+
+  bool showLoading = true;
+
+  String searchText = "";
+
   @override
   void initState() {
+    beforeHook();
     super.initState();
   }
 
-  final AsyncMemoizer _memoizer = AsyncMemoizer();
+  @override
+  void dispose() {
+    _textEditingController.removeListener(handleSearch);
+    _textEditingController.dispose();
+    super.dispose();
+  }
 
-  Future<CookParseDataModel> loadReadme() async {
-    // await Future.delayed(Duration(seconds: 2));
+  beforeHook() async {
+    _textEditingController.addListener(handleSearch);
+    await loadReadme();
+    showLoading = false;
+    setState(() {});
+  }
+
+  handleSearch() {
+    var text = _textEditingController.text;
+    searchText = text;
+    setState(() {});
+  }
+
+  loadReadme() async {
+    await Future.delayed(const Duration(seconds: 2));
     String raw = await _memoizer.runOnce(() async {
       return await rootBundle.loadString(joinMarkdownPath('README.md'));
     });
-    var data = tryParse(raw);
-    return data;
+    data = tryParse(raw);
+    setState(() {});
   }
 
   tryParse(String raw) {
     CoreParse coreParse = CoreParse();
     coreParse.parseMetaData(raw);
     return coreParse.data;
+  }
+
+  List<Before> easyFilterWithBefore(List<Before> old) {
+    if (searchText.isEmpty) return old;
+    return old.where((element) {
+      return element.title.contains(searchText);
+    }).toList();
+  }
+
+  ColCookList easyFilterWithColData(ColCookList old) {
+    if (searchText.isEmpty) return old;
+    ColCookList output = {};
+    for (var element in old.entries) {
+      var K = element.key;
+      var V = element.value;
+      if (output[K] == null) {
+        output[K] = [];
+      }
+      var matchResult = V.where((element) {
+        return element.title.contains(searchText);
+      }).toList();
+      output[K]!.addAll(matchResult);
+    }
+    return output;
   }
 
   @override
@@ -45,23 +98,15 @@ class _HomePageState extends State<HomePage> {
         middle: Text("程序员做饭指南"),
       ),
       child: SafeArea(
-        child: FutureBuilder<CookParseDataModel>(
-          future: loadReadme(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState != ConnectionState.done) {
+        child: Builder(
+          builder: (context) {
+            if (showLoading) {
               return const Center(
                 child: CupertinoActivityIndicator(),
               );
             }
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text(
-                  "load fail!",
-                  style: TextStyle(color: CupertinoColors.systemPink),
-                ),
-              );
-            }
-            var data = snapshot.data;
+            var before = easyFilterWithBefore(data!.before);
+            var colData = easyFilterWithColData(data!.data);
             return Padding(
               padding: const EdgeInsets.symmetric(
                 horizontal: 12.0,
@@ -70,7 +115,9 @@ class _HomePageState extends State<HomePage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const SizedBox(height: 12.0),
-                  const CupertinoSearchTextField(),
+                  CupertinoSearchTextField(
+                    controller: _textEditingController,
+                  ),
                   const SizedBox(height: 6.0),
                   Expanded(
                     child: OverflowBox(
@@ -78,49 +125,58 @@ class _HomePageState extends State<HomePage> {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            const Padding(
-                              padding: EdgeInsets.symmetric(
-                                horizontal: 4.2,
-                                vertical: 12.0,
-                              ),
-                              child: Text(
-                                "做菜之前",
-                                style: TextStyle(
-                                  fontSize: 14.0,
-                                  color: CupertinoColors.systemGrey,
-                                ),
-                              ),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                color: CupertinoTheme.of(context)
-                                    .barBackgroundColor,
-                                borderRadius: BorderRadius.circular(7.2),
-                              ),
-                              padding: const EdgeInsets.symmetric(
-                                vertical: 9.0,
-                                horizontal: 3.0,
-                              ),
-                              child: Column(
-                                children: List.generate(
-                                  data!.before.length,
-                                  (index) => CupertinoListTile(
-                                    onTap: () {
-                                      var curr = data.before[index];
-                                      var link = curr.link;
-                                      context.push('/detail?link=$link');
-                                    },
-                                    dense: true,
-                                    title: Text(
-                                      data.before[index].title,
+                            if (before.isNotEmpty)
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  const Padding(
+                                    padding: EdgeInsets.symmetric(
+                                      horizontal: 4.2,
+                                      vertical: 12.0,
+                                    ),
+                                    child: Text(
+                                      "做菜之前",
+                                      style: TextStyle(
+                                        fontSize: 14.0,
+                                        color: CupertinoColors.systemGrey,
+                                      ),
                                     ),
                                   ),
-                                ),
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      color: CupertinoTheme.of(context)
+                                          .barBackgroundColor,
+                                      borderRadius: BorderRadius.circular(7.2),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 9.0,
+                                      horizontal: 3.0,
+                                    ),
+                                    child: Column(
+                                      children: List.generate(
+                                        before.length,
+                                        (index) => CupertinoListTile(
+                                          onTap: () {
+                                            var curr = before[index];
+                                            var link = curr.link;
+                                            context.push('/detail?link=$link');
+                                          },
+                                          dense: true,
+                                          title: Text(
+                                            before[index].title,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ),
                             Column(
-                              children: data.data.entries.map((e) {
-                                String category = e.key.replaceFirst('### ', '');
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: colData.entries.map((e) {
+                                if (e.value.isEmpty) return const SizedBox();
+                                String category =
+                                    e.key.replaceFirst('### ', '');
                                 return Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: [
